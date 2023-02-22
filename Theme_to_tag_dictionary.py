cd9b4eff1048786025a12ccdf542e9255be631ef
johnandemeskel <jandemeskel@gmail.com>
2023-02-22 19:02:09 +0000

    Theme:tag dictionary created - need clarificaiton

---
+++ Theme_to_tag_dictionary.py
@@ -0,0 +1,35 @@
+Theme_to_tag = 
+{ "SOIO": 
+[
+    "Scope of international obligations",
+    "Cooperation with human rights mechanisms & requests for technical assistance", 
+    "Inter-State cooperation and assistance", 
+    "Ratification of & accession to international instruments", 
+    "Reservations", "Derogation & state of emergency", 
+    "Cooperation & follow up with Treaty Bodies", 
+    "Cooperation & follow up with Special Procedures", 
+    "Cooperation & follow up with the Universal Periodic Review (UPR)", 
+    "Cooperation with international organizations"
+],
+"NHRF": 
+[ 
+    "Legal and institutional reform",
+    "Human rights education, trainings & awareness-raising",
+    "Constitutional & legislative framework",
+    "National Human Rights Action Plans (or specific areas) / implementation plans",
+    "National Mechanisms for Reporting & Follow-up (NMRF)",
+    "National Human Rights Institution (NHRI)",
+    "National Preventive Mechanism (NPM)",
+    "Cooperation & consultation with civil society",
+    "Data collection & research",
+    "Budget & resources (for human rights implementation)",
+    "Research & other measures of implementation",
+    "Independent Monitoring Mechanism (IMM)",
+],
+"RSOL":
+[
+    
+]
+
+
+}
