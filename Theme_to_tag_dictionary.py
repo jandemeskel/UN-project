dd25915a9ab7816c969acf1c97fdfa25819cef0b
johnandemeskel <jandemeskel@gmail.com>
2023-02-23 16:06:15 +0000

    verified & complete theme to tag dictionary

--- Theme_to_tag_dictionary.py
+++ Theme_to_tag_dictionary.py
@@ -1,5 +1,5 @@
-Theme_to_tag = 
-{ "SOIO": 
+general_framework = { 
+"Scope of international obligations and cooperation with international human rights mechanisms and bodies": 
 [
     "Scope of international obligations",
     "Cooperation with human rights mechanisms & requests for technical assistance", 
@@ -11,7 +11,7 @@ Theme_to_tag =
     "Cooperation & follow up with the Universal Periodic Review (UPR)", 
     "Cooperation with international organizations"
 ],
-"NHRF": 
+"National human rights framework": 
 [ 
     "Legal and institutional reform",
     "Human rights education, trainings & awareness-raising",
@@ -24,12 +24,130 @@ Theme_to_tag =
     "Data collection & research",
     "Budget & resources (for human rights implementation)",
     "Research & other measures of implementation",
-    "Independent Monitoring Mechanism (IMM)",
-],
-"RSOL":
+    "Independent Monitoring Mechanism (IMM)"
+]}
+
+
+
+cross_cutting_issues = {
+"Development":
 [
-    
-]
+    "Right to development",
+    "Economic policy and foreign debt",
+    "2030 Agenda & other voluntary commitments",
+    "Unilateral coercive measures"
+],
+"Environment":
+[
+    "Human rights & the environment",
+    "Human rights & toxics/hazardous wastes",
+    "Human rights & climate change",
+    "Economic & political crisis, natural disasters or other emergencies"
+],
+"Business and human rights":["Business & Human Rights"],
+"Human rights and counter-terrorism ":["Human rights & counter-terrorism"],
+"Right to land":["Land & property rights"]
+}
+
+
+
+civil_and_political = {
+"Civil & political rights - general measures of implementation":["Civil & political rights - general measures of implementation"],
+"Equality and non-discrimination":
+[
+    "Equality & non-discrimination",
+    "Racial discrimination",
+    "Persons deprived of liberty: definition of torture & ill-treatment",
+    "Persons deprived of liberty: concept of places of deprivation of liberty"
+],
+"Right to life, liberty and security of person":
+[
+    "Right to physical & moral integrity",
+    "Liberty & security of the person",
+    "Right to life",
+    "Extrajudicial, summary or arbitrary executions",
+    "Death penalty",
+    "Prohibition of torture & ill-treatment (including cruel, inhuman or degrading treatment)",
+    "Conditions of detention",
+    "Enforced disappearances",
+    "Arbitrary arrest & detention",
+    "Use of mercenaries/private security",
+    "International criminal & humanitarian law (including crimes against humanity, war crimes, genocide)"
+],
+"Administration of justice, including impunity, and the rule of law":
+[
+    "Good governance & corruption",
+    "Access to justice & remedy",
+    "Rule of law & impunity",
+    "Support to victims & witnesses",
+    "Transitional justice",
+    "Administration of justice & fair trial",
+    "Right to be recognized as a person before the law",
+],
+    "Right to participate in public and political life": ["Right to participate in public affairs & right to vote", "Civil & political rights - general measures of implementation"], 
+    "Freedom of opinion and expression and right to information": ["Freedom of opinion and expression & access to information"], 
+    "Freedom of religion": ["Freedom of thought, conscience & religion"], 
+    "Freedom of association and peaceful assembly": ["Right to peaceful assembly","Freedom of association"],
+    "Freedom of movement": ["Freedom of movement"],
+    "Prohibition of all forms of slavery": ["Human trafficking & contemporary forms of slavery"],
+    "Right to privacy": ["Private life & privacy"], 
+    "Right to  family life": ["Rights related to marriage & family"],
+    "Right to determine their own identity (including name and nationality)": ["Rights related to name, identity & nationality"],
+    "Right to self-determination, autonomy/self-government and maintain their own institutions (including  prioir and informed consent)": ["Right to self-determination"]
 
 
 }
+
+
+
+eco_socio_cultural = {
+"Economic, social & cultural rights - general measures of implementation":["Economic, social & cultural rights - general measures of implementation"],
+"Right to an adequate standard of living":
+[
+    "Right to an adequate standard of living",
+    "Right to adequate housing",
+    "Right to food",
+    "Human rights & poverty",
+    "Safe drinking water & sanitation"
+],
+"Right to work and to just and favourable conditions of work":["Labour rights and right to work", "Trade union rights"],
+    "Right to social security":["Right to social security"],
+    "Right to health and to traditional health practices":["Right to health","Sexual & reproductive health and rights"], 
+    "Right to education and control over educational systems":["Right to education"], 
+    "Right to develop their culture, religion and language and non-assimilation":["Cultural rights"]
+}
+
+
+
+
+specific_persons_and_groups = {
+"Women":
+[
+    "Advancement of women",
+    "Discrimination against women",
+    "Violence against women",
+    "Participation of women in political & public life",
+    "Sexual & gender-based violence"
+],
+"Children":
+[
+    "Children: definition; general principles; protection",
+    "Children: family environment & alternative care",
+    "Children: protection against exploitation",
+    "Children: juvenile justice"
+],
+"Persons with disabilities":
+[
+    "Persons with disabilities: definition, general principles",
+    "Persons with disabilities: accessibility, mobility",
+    "Persons with disabilities: protection against exploitation, violence & abuse",
+    "Persons with disabilities: protecting the integrity of the person",
+    "Persons with disabilities: independence, inclusion",
+    "Persons with disabilities: protection and safety in situations of risk"
+],
+"Migrants, refugees, asylum seekers and internally displaced persons":[""],
+"Stateless persons":[""]
+}
+
+
+
